64693584dfea40e609e2e228b10b871c3df63432
Mierlă Cristina <mierla_cristina@yahoo.com>
2022-03-14 14:16:11 +0200

    Isolating values in Romania

--- Monitorizarea si predictia in schimbarile de temperatura cu efect in topirea ghetarilor/server/main.py
+++ Monitorizarea si predictia in schimbarile de temperatura cu efect in topirea ghetarilor/server/main.py
@@ -7,11 +7,11 @@ import chardet
 filename = "Environment_Temperature_change_E_All_Data_NOFLAG.csv"
 
 if __name__ == '__main__':
-    import chardet
-
-    # with open(filename, 'rb') as rawdata:
-    #     result = chardet.detect(rawdata.read(100000))
-    # print(result)
-    # #
     dataset = pd.read_csv(filename, encoding='Windows-1252')
     print(dataset.head())
+    print(dataset.shape)
+
+    romaniaData = dataset[dataset["Area"] == "Romania"]
+    print(romaniaData.head())
+    print(romaniaData.shape)
+
