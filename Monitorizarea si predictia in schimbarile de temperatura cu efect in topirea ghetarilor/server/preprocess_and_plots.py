a3dfe48a861974aa5f13579286654187aa053892
alisamaier <maieralisa0@gmail.com>
2022-03-31 19:36:51 +0300

    line of regression

--- Monitorizarea si predictia in schimbarile de temperatura cu efect in topirea ghetarilor/server/preprocess_and_plots.py
+++ Monitorizarea si predictia in schimbarile de temperatura cu efect in topirea ghetarilor/server/preprocess_and_plots.py
@@ -1,6 +1,7 @@
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def preprocess(df: DataFrame) -> DataFrame:
@@ -34,4 +35,36 @@ def plotByMonth(df: DataFrame, month: str):
     plt.ylabel('Temperature change')
     plt.legend()
     plt.title('temp change in Romania')
+    plt.show()
+
+def lineOfRegression():
+    data = pd.read_csv('RomaniaData.csv', header=None)
+    y = np.array(data.iloc[:, :-1].values)
+    y = np.array([a[0] for a in y])
+    arr = np.array(data.iloc[:, -1].values)
+    x = np.empty(0)
+    for elem in arr:
+        day, month, year = map(int, elem.split("/"))
+        dates = int(year * 100 + month)
+        x = np.append(x, dates)
+    n = np.size(arr)
+
+    y_mean = np.mean(y)
+    x_mean = np.mean(x)
+
+    Sxy = np.sum(x * y) - n * x_mean * y_mean
+    Sxx = np.sum(x * x) - n * x_mean * x_mean
+
+    b1 = Sxy / Sxx
+    b0 = y_mean - b1 * x_mean
+    #print('slope b1 is', b1)
+    #print('intercept b0 is', b0)
+
+    y_pred = b1 * x + b0
+
+    plt.scatter(x, y, color='red')
+    plt.plot(x, y_pred, color='green')
+    plt.xlabel('Year')
+    plt.ylabel('Temperature change')
+    plt.title("Line of Regression")
     plt.show()
